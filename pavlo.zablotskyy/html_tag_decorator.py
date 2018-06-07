5f0145fe8a882f8f5c7409ab28d687728c3d9537
Pavlo <pavlo.zablotskyy@gmail.com>
2018-06-07 21:10:01 +0300

    Added escaping of '<','>', ", "&"

---
+++ pavlo.zablotskyy/html_tag_decorator.py
@@ -0,0 +1,16 @@
+
+SYMBOLS_TO_ESCAPE = {"<":"&lt",
+					 ">":"&gt",
+					 "&":"&amp",
+					 "\"":"&quot"}
+
+def escape_html_tags(html_line):
+	escaped_line = ""
+	for symbol in html_line:
+		if symbol in SYMBOLS_TO_ESCAPE:
+			escaped_line += SYMBOLS_TO_ESCAPE[symbol]
+		else:
+			escaped_line += symbol
+	return escaped_line
+
+print(escape_html_tags("$@<-asdf>&"))
