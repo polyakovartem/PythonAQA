32facf4992b52f609002b6371e8c5541add3d9b9
Yuriy Zhuk <yurakekc@hotmail.com>
2018-06-11 10:53:14 +0300

    homework(html tags)

--- y_zhuk/first_homework.py
+++ y_zhuk/first_homework.py
@@ -1,27 +1,29 @@
-import re
-from functools import wraps
+dict_with_tags = {'&': '&amp;',
+                  '"': '&quot;',
+                  '<': '&lt;',
+                  '>': '&gt;'}
 
 
-pattern = re.compile(r"(?<=\>)(\w+[\w*\s*]+)(?=\<\/)")
+def html_escaper(func):
+    """
+    Decorator function which escape HTML tags
+    :param func: (function)
+    :return: text with escaped HTML-tags
+    """
+    def wrapper(*args, **kwargs):
+        input_text = func(*args, **kwargs)
+        return ''.join(
+            [dict_with_tags[x] if x in dict_with_tags
+             else x for x in input_text])
+    return wrapper
 
 
-def html_escaper():
-    def decorator(func):
-        @ wraps(func)
-        def wrapper(*a, **k):
-            value = func(*a, **k)
-            data = re.findall(pattern, value)
-            if data:
-                return data[0]
-            else:
-                raise Exception(mssg="Data isn't find")
-        return wrapper
-    return decorator
+@html_escaper
+def html_text(input_text):
+    """
+    This function escape html tags
+    """
+    return input_text
 
 
-@html_escaper()
-def escape_html_func(data):
-    return data
-
-
-print(escape_html_func("<div class='modal'>some text inside the div</div>"))
+print(html_text("<div class='modal'>some text inside the div</div>"))
