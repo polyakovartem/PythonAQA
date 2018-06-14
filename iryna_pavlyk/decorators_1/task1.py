d962f706ae82787c4a5b34b41d9a5f2f605cca3d
Iryna Pavlyk <iryna.pavlyk@ericsson.com>
2018-06-14 20:09:36 +0300

    edit first homework

---
+++ iryna_pavlyk/decorators_1/task1.py
@@ -0,0 +1,55 @@
+"""
+This module describes a homework related to homework with decorators.
+"""
+__author__ = "Iryna Pavlyk"
+__email__ = "iruska.m1@ukr.net"
+
+from functools import wraps
+
+
+mapping = {'&': '&amp;',
+           '"': '&quot;',
+           '<': '&lt;',
+           '>': '&gt;'}
+
+
+def escape_html():
+    """This function as a decorator function handles input text
+    (as *args from function to wrap) and escaped html tags from it
+
+    Args:
+         function(func) - function to wrap
+
+    Returns:
+        func_value(str) - text without html tags
+    """
+    def real_decorator(function):
+        @wraps(function)
+        def wrapper(*args, **kwargs):
+            func_value = function(*args, **kwargs)
+            for el in mapping:
+                func_value = func_value.replace(el, mapping[el])
+            return func_value
+        return wrapper
+    return real_decorator
+
+
+@escape_html()
+def handle_text_html(input_data):
+    """This function escape html tags from input string using
+    'escape_html' decorator function
+
+    Args:
+        input_data(str) - text, that need to be handled
+
+    Returns:
+        input_data(str) - text, that need to be handled
+         """
+    return input_data
+
+
+print(handle_text_html('<f><tt>text</ttt></>'))
+print(handle_text_html('<f><tt>text test</ttt></>'))
+print(handle_text_html('<f><tt>tex"t t"est text2</ttt></>'))
+print(handle_text_html('<f><tt>text& test text uui</ttt></>'))
+print(handle_text_html('<f><tt>text test 3333>3 text uui fff ddd</ttt></>'))
---
+++ iryna_pavlyk/decorators_1/task_2.py
@@ -0,0 +1,49 @@
+"""
+This module describes a homework related to homework with decorators.
+"""
+__author__ = "Iryna Pavlyk"
+__email__ = "iruska.m1@ukr.net"
+
+import random
+from functools import wraps
+
+
+tags = ["a", "li", "form", "i", "b", "aside", "iframe",
+        "div", "body", "html", "ul", "h2"]  # this is abstract list with tags
+
+
+def add_html_tags():
+    """This function as a decorator function handles input text
+    (as *args from function to wrap) and wrapped it into html tags.
+
+    Args:
+         function(func) - function to wrap
+
+    Returns:
+         output_data(str) - text, that is wrapped into html tags
+    """
+    def real_decorator(function):
+        @wraps(function)
+        def wrapper(*args, **kwargs):
+            func_value = function(*args, **kwargs)
+            tag_1 = random.choice(tags)  # get random tag
+            tag_2 = random.choice(tags)  # get random tag
+            output_data = "<{}><{}>{}</{}></{}>".format(
+                tag_1, tag_2, func_value, tag_2, tag_1,)
+            return output_data
+        return wrapper
+    return real_decorator
+
+
+@add_html_tags()
+def wrap_text_html(input_data):
+    """This function wrap a text into html tags
+    Args:
+        input_data(str) - text, that need to be wrapped
+
+    Returns:
+        input_data(str) - text, that need to be wrapped
+    """
+    return input_data
+
+print(wrap_text_html("to be or not to be, that is the question"))
