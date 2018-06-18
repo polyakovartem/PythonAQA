607627fef6ab900ba07e5f9b5da52d28d7b3bf12
AnyaShymanska <ani4ka30@gmail.com>
2018-06-19 01:53:11 +0300

    Add SOLID homework

--- hanna_shymanska/S.O.L.I.D./SRP_solid.py
+++ hanna_shymanska/S.O.L.I.D./SRP_solid.py
@@ -6,24 +6,19 @@ class Charge:
     def pay():
         print('Payment for all products')
 
+
 class Mailing:
     @staticmethod
     def send_mail():
         print('This is an email about order')
+
 
 class Sale:
     @staticmethod
     def generate_code():
         print('Generating code for each customer')
 
-class Order(Charge, Mailing, Sale):
-    @staticmethod
-    def final_order():
-        print("It's your final order")
+
 Charge.pay()
 Mailing.send_mail()
 Sale.generate_code()
-Order.final_order()
-Order.pay()
-Order.send_mail()
-Order.generate_code()
