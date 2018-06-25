24ac0cfa042e7eb45e00881111eac6e4ebff432b
Yuriy Zhuk <yurakekc@hotmail.com>
2018-06-25 16:16:03 +0300

    Update wget analog

--- y_zhuk/homeworks/homework_third/wget.py
+++ y_zhuk/homeworks/homework_third/wget.py
@@ -1,45 +1,63 @@
 import re
 import sys
 import urllib.request
-from abc import abstractmethod
+from collections import Counter
 
 
-class Preparation(object):
-
-    @abstractmethod
-    def check_argv(self, argument):
-        pass
+# file_url = "https://www.w3.org/TR/PNG/iso_8859-1.txt"
+# site_url = "https://habr.com"
+pattern = re.compile(r"[a-zA-Z_]+")
 
 
-class Wget(Preparation):
+class CheckSystemArguments:
 
-    def check_argv(self, argument):
+    @staticmethod
+    def check_argv(argument):
         if len(sys.argv) >= 2:
             return argument
         else:
             raise Exception(
                 mssg='pass the arguments into command line')
 
-    @staticmethod
-    def count_words(text):
-        count = len(re.findall("[a-zA-Z_]+", text))
-        return count
+
+class WorkWithHtmlFile:
 
     @staticmethod
     def open_html_file(url):
         try:
             with urllib.request.urlopen(url) as f:
                 html = f.read().decode(encoding='UTF-8')
-                print("This page has:", Wget.count_words(html), "words.")
+                return html
         except (OSError, RuntimeError):
             print("Error: File does not appear to exist.")
             return []
 
 
+class WordCount:
+    def __init__(self, file_content):
+        self.file_content = file_content
+
+    def get_count(self):
+        count = len(re.findall(pattern, self.file_content))
+        return count
+
+
+class WordStatistics:
+
+    def __init__(self, file_content):
+        self.file_content = file_content
+
+    def get_statistic(self):
+        splitted_data = re.findall(pattern, self.file_content)
+        statistic = Counter(splitted_data).most_common(50)
+        return statistic
+
+
 def main():
-    wget = Wget()
-    html = wget.check_argv(sys.argv[1])
-    wget.open_html_file(html)
+    URL = CheckSystemArguments.check_argv(sys.argv[1])
+    data = WorkWithHtmlFile.open_html_file(URL)
+    print(WordCount(data).get_count())
+    print(WordStatistics(data).get_statistic())
 
 
 if __name__ == '__main__':
