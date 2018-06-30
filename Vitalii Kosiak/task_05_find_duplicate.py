81051c0a2a3fed36cadc77381fd72ad12afda85c
Vitalii Kosiak <viths@i.ua>
2018-07-01 01:41:24 +0300

    5

--- Vitalii Kosiak/task_05_find_duplicate.py
+++ Vitalii Kosiak/task_05_find_duplicate.py
@@ -1,10 +1,14 @@
 import collections
 from collections import Counter
+
 import os
 import os.path
 
+import hashlib
+
 adress = 'E:/test'
 #name_size = collections.defaultdict(list)
+hashdict = {}
 
 def get_files(adress):
     
@@ -21,16 +25,36 @@ def get_files(adress):
             name_size[fullname].append(os.path.getsize(fullname))
 
     for i in data:
-        print ('{} {} {} \n'.format(str(num), i, data[i]))
+        #print ('{} {} {} \n'.format(str(num), i, data[i]))
         num += 1
 
     num = 1
         
     for i in name_size:
-        print ('{} {} {} \n'.format(str(num), i, name_size[i]))
+        #print ('{} {} {} \n'.format(str(num), i, name_size[i]))
         num += 1
 
+    #hashdict = {}
+    
+    for i in name_size:
+        with open(i, 'r') as f:
+            d = f.read()
+        hashdict[i] = hash(d)
 
-        
-        
+    hashlistn = []
+    hashlistc = []
 
+    for i in hashdict:
+        hashlistn.append(i)
+        hashlistc.append(hashdict[i])
+
+    #print (hashlistn)
+    #print (hashlistc)
+
+    k = 0
+    
+    for i in range(k, len(hashlistc)):
+        for x in range(k + 1, len(hashlistc)):
+            if hashlistc[i] == hashlistc[x]:
+                print (str(hashlistn[i]) + '  ==  ' + str(hashlistn[x]) )
+        k = k + 1
